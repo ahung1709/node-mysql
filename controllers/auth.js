b99ac595d14c08eae9fd9bc66102b4d08f48b586
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2026-03-10 13:58:57 -0700

    Update logic to show profile and logout links only when user is logged in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const { promisify } = require('util');
 
 const db = mysql.createConnection({
   host: process.env.DATABASE_HOST,
@@ -104,4 +105,40 @@ exports.register = (req, res) => {
       );
     },
   );
+};
+
+exports.isLoggedIn = async (req, res, next) => {
+  // console.log(req.cookies);
+  if (req.cookies.jwt) {
+    try {
+      // 1) verify the token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET,
+      );
+
+      console.log(decoded);
+
+      // 2) Check if the user still exists
+      db.query(
+        'SELECT * FROM users WHERE id = ?',
+        [decoded.id],
+        (error, result) => {
+          console.log(result);
+
+          if (!result) {
+            return next();
+          }
+
+          req.user = result[0];
+          return next();
+        },
+      );
+    } catch (error) {
+      console.log(error);
+      return next();
+    }
+  } else {
+    next();
+  }
 };
